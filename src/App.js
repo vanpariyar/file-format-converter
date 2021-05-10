fb32f089f7f2fa562c7028ccdb8637b959efa0e0
Ronak Vanpariya <vanpariyar@gmail.com>
2021-05-10 12:35:43 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+
+import Mp4ToGif from './components/mp4ToGif';
 
 function App() {
   return (
     <div className="App">
-      
+      <Mp4ToGif />
     </div>
   );
 }
